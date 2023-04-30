49d96f608208809cb827fbf97737254b27ceb779
Sadlize <sadlize@mail.ru>
2023-04-30 12:00:00 +0300

    update dependencies versions

--- src/redux/ReduxProvider.tsx
+++ src/redux/ReduxProvider.tsx
@@ -4,6 +4,8 @@ import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 
-export default function ReduxProvider({ children }: PropsWithChildren) {
+function ReduxProvider({ children }: PropsWithChildren) {
   return <Provider store={store}>{children}</Provider>;
 }
+
+export default ReduxProvider;
