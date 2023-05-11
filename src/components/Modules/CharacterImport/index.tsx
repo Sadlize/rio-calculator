b426c4a3ecbc807474f8dd6404b546851b653605
Sadlize <sadlize@mail.ru>
2023-05-11 05:02:00 +0300

    feat: add styling and search status texts

--- src/components/Modules/CharacterImport/index.tsx
+++ src/components/Modules/CharacterImport/index.tsx
@@ -15,46 +15,86 @@ import styles from './CharacterImport.module.scss';
 type TImportForm = {
   translations: {
     inputPlaceholder: string;
-    // suggestion: string;
+    search: string;
+    empty: string;
   };
 };
 
 function CharacterImport({ translations }: TImportForm) {
   const [characterName, setCharacterName] = useState('');
+  const characterNameMinLength = characterName.length >= 2;
   const [suggestions, setSuggestions] = useState<Array<TSuggestions>>([]);
+  const [searchStatus, setSearchStatus] = useState<
+    undefined | 'start' | 'finish' | 'empty'
+  >(undefined);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const getCharacterSuggestions = async () => {
       const response = await fetch(`api/search/${characterName}`);
       const characters = getCharactersFromSearch(await response.json());
+      if (isEmpty(characters)) {
+        return setSearchStatus('empty');
+      }
       setSuggestions(characters as Array<TSuggestions>);
+      return setSearchStatus('finish');
     };
 
-    if (characterName.length >= 2) {
+    if (characterNameMinLength) {
+      setSearchStatus('start');
       const timeOutId = setTimeout(() => getCharacterSuggestions(), 500);
       return () => clearTimeout(timeOutId);
     }
 
+    setSearchStatus(undefined);
     return setSuggestions([]);
-  }, [characterName]);
+  }, [characterName, characterNameMinLength]);
 
-  const test = (region: string, realm: string, name: string) => async () => {
-    const response = await characterRIOData.get(region, realm, name);
-    if (!response.data) {
-      return;
-    }
+  const importSelectedCharacter =
+    (region: string, realm: string, name: string) => async () => {
+      const response = await characterRIOData.get(region, realm, name);
+      if (!response.data) {
+        return;
+      }
 
-    const data = refactorRIOData(response.data as RIOData);
-    dispatch(setImportScore(data.scores));
-    dispatch(setImportKeyLevel(data.keyLevels));
-    dispatch(setImportTimestamp(data.timestamps));
-  };
+      const data = refactorRIOData(response.data as RIOData);
+      dispatch(setImportScore(data.scores));
+      dispatch(setImportKeyLevel(data.keyLevels));
+      dispatch(setImportTimestamp(data.timestamps));
+
+      setCharacterName('');
+    };
 
   const inputChangeHandle = (event) => {
     const inputValue = event.target.value;
     if (inputValue.length <= 12) {
       setCharacterName(inputValue);
+    }
+  };
+
+  const suggestionsList = () => {
+    switch (searchStatus) {
+      case 'start':
+        return <div>{translations.search}</div>;
+      case 'empty':
+        return <div>{translations.empty}</div>;
+      case 'finish':
+        return suggestions.map((s) => {
+          const { name, data } = s;
+          const { region, realm, id } = data;
+          return (
+            <li key={id}>
+              <Button
+                className={styles.suggestion}
+                onClick={importSelectedCharacter(region.slug, realm.slug, name)}
+              >
+                {`${name} ${region.slug.toUpperCase()}-${realm.name}`}
+              </Button>
+            </li>
+          );
+        });
+      default:
+        return undefined;
     }
   };
 
@@ -69,18 +109,8 @@ function CharacterImport({ translations }: TImportForm) {
           onChange={inputChangeHandle}
         />
       </form>
-      {!isEmpty(suggestions) && (
-        <div className={styles.suggestionsTooltip}>
-          {suggestions.map((s) => {
-            const { name, data } = s;
-            const { region, realm, id } = data;
-            return (
-              <Button key={id} onClick={test(region.slug, realm.slug, name)}>
-                {`${name} ${region.slug.toUpperCase()}-${realm.name}`}
-              </Button>
-            );
-          })}
-        </div>
+      {searchStatus && (
+        <ul className={styles.suggestionsTooltip}>{suggestionsList()}</ul>
       )}
     </div>
   );
